3037e574adcdcbb61eec04c800821964c2e48e98
aviEL <victor@kangaroorewards.com>
2020-03-16 12:43:17 -0400

    initial netbeans project

---
+++ starter/src/main/java/com/aviEL/ELiehad/starter/common/customer.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.aviEL.ELiehad.starter.common;
+import javax.persistance.Entity;
+
+/**
+ *
+ * @author AviEL
+ */
+@Entity
+public class customer {
+    private Long id;
+    private String firstname;
+    private String lastname;
+    
+    
+}
